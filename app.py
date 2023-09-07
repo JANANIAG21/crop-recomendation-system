60b84dc498cfb3d6cdf5a0a1a7296b68552154fd
JANANIAG21 <113996860+JANANIAG21@users.noreply.github.com>
2023-09-07 09:36:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,13 +51,13 @@ with st.sidebar:
     
     load_bootstrap()
     st.markdown("""<h4 style='text-align: center; color: black;'>
-    This Application was developed by Mateus Prevelato Athayde. 
+    This Application was developed by janani . 
     You can find my Linkedin and GitHub profiles below:</h4>""",unsafe_allow_html=True)
     st.markdown(f"""<h4 style='text-align: center; color: black;'>
      <a style='text-align: center; color: black;' type="button" class="btn btn-warning btn-lg" 
-     href = "https://github.com/MPrevelato/Crop_Recommendation_System">GitHub</a> <a style='text-align: center; color: black;' 
+     href = "https://github.com/JANANIAG21/crop-recomendation-system">GitHub</a> <a style='text-align: center; color: black;' 
      type="button" class="btn btn-warning btn-lg" 
-     href = "https://www.linkedin.com/in/mateus-prevelato/">Linkedin</a></h4>""", unsafe_allow_html=True)
+     href = "https://www.linkedin.com/in/janani-dhanabalan-8a8560233/">Linkedin</a></h4>""", unsafe_allow_html=True)
 
 st.markdown("<h1 style='text-align: center; color: black;'>Crop Recommendation System</h1>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center;'>This Application predict what is the best crop to plant based on NPK values and Weather Conditions!</h5>", unsafe_allow_html= True)
